bd9171ac03410fc46742723f97e608e11a8bffd4
Rev <oriaviad1@gmail.com>
2023-09-22 01:01:37 +0300

    added buttons

---
+++ popup/popup.js
@@ -0,0 +1,14 @@
+// ELEMENTS
+const phoneNumberIDElement = document.getElementById("phoneID")
+const TimeOfDayIDElement = document.getElementById("time of day")
+
+//Buttons elements
+const startButton = document.getElementById("startButton")
+const stopButton = document.getElementById("stopButton")
+
+startButton.onclick = function(){
+    console.log("you clicked start")
+}
+stopButton.onclick = function(){
+    console.log("you clicked stop")
+}
