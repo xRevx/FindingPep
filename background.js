ccf4f044e5c2404d7b4d4fe7f195d9c2f8b1aad9
Rev <oriaviad1@gmail.com>
2023-09-23 02:35:48 +0300

    trying to send message with python

--- background.js
+++ background.js
@@ -1,3 +1,10 @@
+
+var port = chrome.runtime.connectNative("native_message_host");
+
+function callSendHandle(data) {
+  port.postMessage({ action: "call_send_handle", data: data });
+}
+
 chrome.runtime.onInstalled.addListener(
     details =>{
         console.log("onInstall reason: ", details.reason)
@@ -23,12 +30,25 @@ chrome.runtime.onMessage.addListener(data => {
 })
 
 const handleOnStart = (prefs) => {
+    
     console.log("on start bg")
     console.log("data sent", prefs)
+    send_handle(prefs);
     chrome.storage.local.set(prefs)
 }
 
 const handleOnStop = () => {
     console.log("on stop bg")
 }
+
+function send_handle(prefs){
+    var normalizedNumber = prefs.phoneNumber.replace(/\D/g, '');
+    // normalizedNumber = '+' + normalizedNumber;
+    console.log("normalized",normalizedNumber)
+    console.log("message",prefs.Message)
+
+    var whatsappUrl = `https://wa.me/${normalizedNumber}?text=I'm%20interested%20in%20your%20car%20for%20sale`;
+    chrome.tabs.create({url: whatsappUrl});
+   // win.focus();
+  }
 
